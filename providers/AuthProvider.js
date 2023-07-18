15c5a017693547e3a4b9029acbf6545b5b08cd19
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 00:48:43 +0100

    feat: create AuthProvider

---
+++ providers/AuthProvider.js
@@ -0,0 +1,9 @@
+import { SessionProvider } from 'next-auth/react';
+
+const AuthProvider = ({ children }) => {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  );
+};
+
+export default AuthProvider;
