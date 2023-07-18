31dbabffe6a673c32d039c4dd0976eb8e1aa30a9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 00:22:42 +0100

    fix: add markup

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-blue-900 w-screen h-screen flex items-center'>
+      <div className='text-center w-full'>
+        <button className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+      </div>
+    </div>
   );
 };
 
