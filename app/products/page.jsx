b730841ccaa1ba33f06ffdda0b1d42ea2d417a50
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 16:22:58 +0100

    fix: add markup

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 const Products = () => {
   return (
     <div>
-      Products
+      <Link
+        className='bg-blue-900 text-white rounded-md py-1 px-2'
+        href='/products/new'
+        passHref
+      >
+        Add new product
+      </Link>
     </div>
   );
 };
