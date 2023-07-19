85f64e56e85f9e563f7ee275ba4cecc4a9e48ae9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 18:26:22 +0100

    feat: add a product model schema

---
+++ models/Product.js
@@ -0,0 +1,37 @@
+import slugify from 'slugify';
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'A product must have a title'],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'A product must have a description'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A product must have a price'],
+  },
+});
+
+productSchema.pre('save', async function (next) {
+  if (!this.isModified('title')) return next();
+  this.slug = slugify(this.title, { lower: true });
+
+  const regEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const productWithSlug = await this.constructor.find({ slug: regEx });
+
+  if (productWithSlug.length) {
+    this.slug = `${this.slug}-${productWithSlug.length + 1}`;
+  }
+});
+
+const Product = mongoose.models.Product ||
+  mongoose.model('Product', productSchema);
+
+export default Product;
