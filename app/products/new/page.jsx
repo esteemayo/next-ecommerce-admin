f5fb9ae12a194aefe695414f3077c008722915d8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 16:47:22 +0100

    fix: add useSatte react hook

--- app/products/new/page.jsx
+++ app/products/new/page.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useSate } from 'react';
 
 const NewProduct = () => {
   return (
-    <div>NewProduct</div>
+    <div>
+      <h1>New Product</h1>
+      <div>
+        <label htmlFor='name'>Product name</label>
+        <input type='text' id='name' placeholder='Product name' />
+      </div>
+      <div>
+        <label htmlFor='description'>Description</label>
+        <textarea id='description' placeholder='Description' />
+      </div>
+      <div>
+        <label htmlFor='price'>Price (in USD)</label>
+        <input id='price' type='number' placeholder='Price' />
+      </div>
+      <button type='submit' className='btn-primary'>Save</button>
+    </div>
   );
 };
 
