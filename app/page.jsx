2dce43710fd72416fc1b52696e1036b2bd2ade7f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 16:03:05 +0100

    fix: add markup

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { useSession } from 'next-auth/react';
+
 import Layout from '@/components/Layout';
 
 const Home = () => {
@@ -8,8 +10,21 @@ const Home = () => {
 
   return (
     <Layout>
-      <div className='text-blue-900'>
-        Hello, {session?.user.email}
+      <div className='text-blue-900 flex justify-between'>
+        <h2>Hello, {session?.user?.name}</h2>
+        <div className='flex align-middle gap-1 bg-gray-300 text-black rounded-lg overflow-hidden'>
+          <Image
+            src='https://randomuser.me/api/portraits/thumb/men/32.jpg'
+            width={6}
+            height={6}
+            alt=''
+            className='relative w-6 h-6'
+          />
+          <span className='px-2'>
+            John Doe
+            {session?.user?.name}
+          </span>
+        </div>
       </div>
     </Layout>
   );
