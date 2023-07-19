faf15e27c8c2d77af26569d6a4f2d6f9ab2b8c4d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 17:02:19 +0100

    fix: modify

--- app/products/new/page.jsx
+++ app/products/new/page.jsx
@@ -21,15 +21,29 @@ const NewProduct = () => {
       <h1>New Product</h1>
       <div>
         <label htmlFor='name'>Product name</label>
-        <input type='text' id='name' placeholder='Product name' onChange={handleChange} />
+        <input
+          type='text'
+          id='name'
+          placeholder='Product name'
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor='description'>Description</label>
-        <textarea id='description' placeholder='Description' onChange={handleChange} />
+        <textarea
+          id='description'
+          placeholder='Description'
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor='price'>Price (in USD)</label>
-        <input id='price' type='number' placeholder='Price' onChange={handleChange} />
+        <input
+          id='price'
+          type='number'
+          placeholder='Price'
+          onChange={handleChange}
+        />
       </div>
       <button type='submit' className='btn-primary'>Save</button>
     </div>
