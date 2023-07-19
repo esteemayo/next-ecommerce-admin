4be332b39a127472364664742ae0caee2119ba4a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 17:22:41 +0100

    feat: configure axios

---
+++ services/httpService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import logger from './logService';
+
+const API = axios.create({
+  baseURL: '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+API.interceptors.response.use(null, (error) => {
+  const expectedError = error.response &&
+    error.response.status > 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: API.get,
+  post: API.post,
+  patch: API.patch,
+  delete: API.delete,
+};
+
+export default http;
