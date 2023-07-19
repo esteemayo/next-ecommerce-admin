dde48ae922aa00d380863e5a4f276adf3eee6abb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 15:39:39 +0100

    fix: add markup

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
 'use client';
 
-import Layout from "@/components/Layout";
+import { useSession } from 'next-auth/react';
+import Layout from '@/components/Layout';
 
 const Home = () => {
+  const { data: session } = useSession();
+
   return (
     <Layout>
-      Home
+      <div className='text-blue-900'>
+        Hello, {session?.user.email}
+      </div>
     </Layout>
   );
 };
