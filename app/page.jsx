f9db1d689348237ae1b81ed2bcc5efb817510068
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 02:07:02 +0100

    fix: rename component

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,27 @@
 'use client';
 
 import { signIn, useSession } from 'next-auth/react';
-import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const Home = () => {
   const { data: session } = useSession();
 
-  if (!session) {
-    return (
-      <div className='bg-blue-900 w-screen h-screen flex items-center'>
-        <div className='text-center w-full'>
-          <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
+  // if (!session) {
+  //   return (
+  //     <div className='bg-blue-900 w-screen h-screen flex items-center'>
+  //       <div className='text-center w-full'>
+  //         <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>
+  //           Login with Google
+  //         </button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className='bg-blue-900 min-h-screen'>
-      <Navbar />
-      <div>Logged in {session.user.email}</div>
+      <Sidebar />
+      <div>Logged in {session?.user.email}</div>
     </div>
   );
 };
