786f639813cac376efc6ec0c126c13e7c94d0511
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 16:18:21 +0100

    fix: refactor

--- app/page.jsx
+++ app/page.jsx
@@ -9,24 +9,22 @@ const Home = () => {
   const { data: session } = useSession();
 
   return (
-    <Layout>
-      <div className='text-blue-900 flex justify-between'>
-        <h2>Hello, <b>{session?.user?.name}</b></h2>
-        <div className='flex align-middle gap-1 bg-gray-300 text-black rounded-lg overflow-hidden'>
-          <Image
-            src='https://randomuser.me/api/portraits/thumb/men/32.jpg'
-            width={6}
-            height={6}
-            alt=''
-            className='relative w-6 h-6'
-          />
-          <span className='px-2'>
-            John Doe
-            {session?.user?.name}
-          </span>
-        </div>
+    <div className='text-blue-900 flex justify-between'>
+      <h2>Hello, <b>{session?.user?.name}</b></h2>
+      <div className='flex align-middle gap-1 bg-gray-300 text-black rounded-lg overflow-hidden'>
+        <Image
+          src='https://randomuser.me/api/portraits/thumb/men/32.jpg'
+          width={6}
+          height={6}
+          alt=''
+          className='relative w-6 h-6'
+        />
+        <span className='px-2'>
+          John Doe
+          {session?.user?.name}
+        </span>
       </div>
-    </Layout>
+    </div>
   );
 };
 
