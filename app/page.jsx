99af0c8d6b0aa42dc3abe7c4f0e0363800224631
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-19 15:17:04 +0100

    fix: refactor

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,12 @@
 'use client';
 
-import { signIn, useSession } from 'next-auth/react';
-import Sidebar from '@/components/Sidebar';
+import Layout from "@/components/Layout";
 
 const Home = () => {
-  const { data: session } = useSession();
-
-  // if (!session) {
-  //   return (
-  //     <div className='bg-blue-900 w-screen h-screen flex items-center'>
-  //       <div className='text-center w-full'>
-  //         <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>
-  //           Login with Google
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
   return (
-    <div className='bg-blue-900 min-h-screen flex'>
-      <Sidebar />
-      <div className='bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
-        Logged in {session?.user.email}
-      </div>
-    </div>
+    <Layout>
+      Home
+    </Layout>
   );
 };
 
