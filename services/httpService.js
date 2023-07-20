c34e389f51b8f23eb08775d6acfaaa434712c284
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 01:41:30 +0100

    fix: fix a bug

--- services/httpService.js
+++ services/httpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import logger from './logService';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
