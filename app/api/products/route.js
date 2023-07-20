9202efa4ceddc3d8ebcb54aa6b710385ea56b35d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 02:14:46 +0100

    fix: handle fetch all products functionality

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,6 +3,22 @@ import { NextResponse } from 'next/server';
 import connectDB from '@/utils/db';
 import Product from '@/models/Product';
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const products = await Product.find();
+
+    return NextResponse.json(products, {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
+
 export const POST = async (request) => {
   const body = await request.json();
 
