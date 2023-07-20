10d6845a918fd1381f5d0828e00d116a142d1e65
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 16:31:55 +0100

    fix: refactor

--- app/products/new/page.jsx
+++ app/products/new/page.jsx
@@ -1,70 +1,13 @@
 'use client';
 
-import { redirect } from 'next/navigation';
-import { useCallback, useState } from 'react';
-
-import { createProduct } from '@/services/productService';
-
-const initialState = {
-  title: '',
-  description: '',
-  price: '',
-};
+import ProductForm from '@/components/ProductForm';
 
 const NewProduct = () => {
-  const [values, setValues] = useState(initialState);
-  const [goToProducts, setGoToProducts] = useState(false);
-
-  const handleChange = useCallback(({ target: input }) => {
-    const { name, value } = input;
-    setValues((prev) => ({ ...prev, [name]: value }));
-  }, []);
-
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault();
-
-    await createProduct({ ...values });
-    setGoToProducts(true);
-  }, [values]);
-
-  if (goToProducts) {
-    return redirect('/products');
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <h1>New Product</h1>
-      <div>
-        <label htmlFor='title'>Product name</label>
-        <input
-          type='text'
-          id='title'
-          name='title'
-          placeholder='Product name'
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor='description'>Description</label>
-        <textarea
-          id='description'
-          name='description'
-          placeholder='Description'
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor='price'>Price (in USD)</label>
-        <input
-          id='price'
-          name='price'
-          type='number'
-          placeholder='Price'
-          onChange={handleChange}
-        />
-      </div>
-      <button type='submit' className='btn-primary'>Save</button>
-    </form>
+      <ProductForm />
+    </>
   );
 };
 
