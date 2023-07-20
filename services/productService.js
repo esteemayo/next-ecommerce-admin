204a0b319bb649c2ecfa75cef46c299053db6ddd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 16:52:05 +0100

    fix: add a function to upda

--- services/productService.js
+++ services/productService.js
@@ -2,10 +2,12 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
+const productUrl = (productId) => `${apiEndpoint}/${productId}`;
+
 export const getProducts = () => http.get(apiEndpoint);
 
 export const getProductById = (productId) =>
-  http.get(`${apiEndpoint}/${productId}`);
+  http.get(productUrl(productId));
 
 export const getProductBySlug = (slug) =>
   http.get(`${apiEndpoint}/details/${slug}`);
@@ -13,4 +15,4 @@ export const getProductBySlug = (slug) =>
 export const createProduct = (product) => http.post(apiEndpoint, product);
 
 export const updateProduct = (productId, product) =>
-  http.patch(`${apiEndpoint}/${productId}`, product);
+  http.patch(productUrl(productId), product);
