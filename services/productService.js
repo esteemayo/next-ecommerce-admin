23acf898065b2e2f7e9bad8cbbd77bbf5f0da561
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 02:09:34 +0100

    fix: add a function to get all products endpoint

--- services/productService.js
+++ services/productService.js
@@ -2,4 +2,6 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
+export const getProducts = () => http.get(apiEndpoint);
+
 export const createProduct = (product) => http.post(apiEndpoint, product);
