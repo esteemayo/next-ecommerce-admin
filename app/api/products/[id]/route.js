2ef70c42e083d8f0c558b49eaa4e1e24df3bdc02
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 19:26:20 +0100

    fix: handle product update functionality

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -25,3 +25,39 @@ export const GET = async (request, { params }) => {
     });
   }
 };
+
+export const PATCH = async (request, { params }) => {
+  const { id: productId } = params;
+  const body = request.json();
+
+  try {
+    await connectDB();
+
+    if (!productId || typeof productId !== 'string') {
+      throw new Error('Invalid ID');
+    }
+
+    if (body.title) body.slug = slugify(body.title, { lower: true });
+
+    const product = await Product.findByIdAndUpdate(
+      productId,
+      { $set: { ...body } },
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    if (!product) {
+      throw new Error('No product found with the given ID');
+    }
+
+    return NextResponse.json(product, {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
