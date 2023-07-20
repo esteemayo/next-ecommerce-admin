fb6d69ae35501d465173bded606866c9d6d6da7b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 02:41:18 +0100

    fix: add markup

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -24,6 +24,29 @@ const Products = () => {
       >
         Add new product
       </Link>
+      <table className='basic mt-2'>
+        <thead>
+          <tr>
+            <th>Product name</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => {
+            const { _id: id, title, description, price } = item;
+            return (
+              <tr key={id}>
+                <td>{title}</td>
+                <td>
+                  <Link href={`/products/${encodeURIComponent(id)}`} passHref>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
