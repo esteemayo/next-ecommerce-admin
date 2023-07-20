ea03b47e1230e6617dd07e19974101f5ffe939ca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 03:02:55 +0100

    fix: add fetch single product api endpoint

--- services/productService.js
+++ services/productService.js
@@ -4,4 +4,7 @@ const apiEndpoint = '/products';
 
 export const getProducts = () => http.get(apiEndpoint);
 
+export const getProduct = (productId) =>
+  http.get(`${apiEndpoint}/${productId}`);
+
 export const createProduct = (product) => http.post(apiEndpoint, product);
