741b504dc7d839b87e557655200023254db5f3df
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-21 00:54:38 +0100

    fix: add useState react hook

--- app/products/delete/[slug]/page.jsx
+++ app/products/delete/[slug]/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const DeleteProduct = () => {
   const router = useRouter();
