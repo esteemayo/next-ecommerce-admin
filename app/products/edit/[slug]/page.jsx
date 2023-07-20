b04e470c3e7e08d1423f613362f1d202f80dbf19
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 03:07:03 +0100

    fix: import getProductBySlug

--- app/products/edit/[slug]/page.jsx
+++ app/products/edit/[slug]/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { getProductBySlug } from '@/services/productService';
 
 const EditProduct = ({ params }) => {
   const [product, setProduct] = useState({});
