3c409efdedaeaa31fd54836c79018dcc30720b54
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-20 02:17:36 +0100

    feat: import getProducts apiEndpoint

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
+import { getProducts } from '@/services/productService';
+
 const Products = () => {
   return (
     <div>
