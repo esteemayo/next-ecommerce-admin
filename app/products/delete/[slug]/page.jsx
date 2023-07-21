ac33e4755c536b0e8a11b27da84803ce73a31a86
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-21 01:18:49 +0100

    fix: add useCallback react hook

--- app/products/delete/[slug]/page.jsx
+++ app/products/delete/[slug]/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { getProductBySlug } from '@/services/productService';
 
