3214a4431b5197e958c5275a024dcc0bbf9f8546
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-22 21:29:19 +0100

    fix: add markup

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({ name, label, type = 'text', ...rest }) => {
   return (
-    <div>Input</div>
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        {...rest}
+        id={name}
+        type={type}
+        name={name}
+      />
+    </div>
   );
 };
 
