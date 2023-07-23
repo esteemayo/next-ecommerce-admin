6ef6273e6d2b366b479a2d116761ebdbbca7555e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-23 02:02:40 +0100

    fix: modify

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -86,13 +86,13 @@ const ProductForm = ({ slug, product, images: existingImages }) => {
       />
       <div>
         <label htmlFor='photos'>Photos</label>
-        <div className='mb-2'>
+        <div className='mb-2 flex flex-wrap gap-2'>
           {!!images.length && images.map((item, index) => {
-            <div key={index} className='inline-block h-24'>
+            <div key={index} className='h-24'>
               <Image src={item} alt='' />
             </div>
           })}
-          <label htmlFor='photos' className='inline-block w-24 h-24 cursor-pointer flex items-center justify-center gap-1 text-sm text-gray-500 rounded-lg bg-gray-200'>
+          <label htmlFor='photos' className='w-24 h-24 cursor-pointer flex items-center justify-center gap-1 text-sm text-gray-500 rounded-lg bg-gray-200'>
             <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
               <path strokeLinecap='round' strokeLinejoin='round' d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5' />
             </svg>
