2e05260f18551acc0b99579bd15b09c5dcc509c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-24 02:24:21 +0100

    fix: handle fetch all categories api logic

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -3,6 +3,22 @@ import { NextResponse } from 'next/server';
 import connectDB from '@/utils/db';
 import Category from '@/models/Category';
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const categories = await Category.find();
+
+    return NextResponse.json(categories, {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
+
 export const POST = async (request) => {
   const body = await request.json();
 
