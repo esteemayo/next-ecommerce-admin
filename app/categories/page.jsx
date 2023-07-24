81bf04158496f1ba57cbb987b81aef3ab0967159
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-24 01:37:42 +0100

    fix: add handleSubmit handler function

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -10,16 +10,18 @@ const Categories = () => {
   return (
     <>
       <h1>Categories</h1>
-      <label htmlFor='name'>New category name</label>
-      <div className='flex gap-1'>
-        <input
-          id='name'
-          type='text'
-          placeholder='Category name'
-          className='mb-0'
-        />
-        <button type='submit' className='btn-primary py-1'>Save</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>New category name</label>
+        <div className='flex gap-1'>
+          <input
+            id='name'
+            type='text'
+            placeholder='Category name'
+            className='mb-0'
+          />
+          <button type='submit' className='btn-primary py-1'>Save</button>
+        </div>
+      </form>
     </>
   );
 };
