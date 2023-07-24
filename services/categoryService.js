ea09064a4ac7d54c41f0ad18406284e732185266
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-24 04:35:12 +0100

    feat: add updateCategory api endpoint function

--- services/categoryService.js
+++ services/categoryService.js
@@ -5,3 +5,6 @@ const apiEndpoint = '/categories';
 export const getCategories = () => http.get(apiEndpoint);
 
 export const createCategory = (name) => http.post(apiEndpoint, name);
+
+export const updateCategory = (categoryId, data) =>
+  http.patch(`${apiEndpoint}/${categoryId}`, data);
