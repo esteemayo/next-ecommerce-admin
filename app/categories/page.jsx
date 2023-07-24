5759cc5411bbb69c84bb9bc9da7651ca496bb2a9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-24 17:12:07 +0100

    fix: import deleteCategory

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -2,7 +2,7 @@
 
 import { useCallback, useEffect, useState } from 'react';
 import { withSwal } from 'react-sweetalert2';
-import { createCategory, getCategories, updateCategory } from '@/services/categoryService';
+import { createCategory, deleteCategory, getCategories, updateCategory } from '@/services/categoryService';
 
 const Categories = () => {
   const [name, setName] = useState('');
