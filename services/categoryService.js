7bc6865efa984602e15f3c4849424fe911b33fc8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-24 17:06:18 +0100

    fix: refactor

--- services/categoryService.js
+++ services/categoryService.js
@@ -2,9 +2,11 @@ import http from './httpService';
 
 const apiEndpoint = '/categories';
 
+const categoryUrl = (categoryId) => `${apiEndpoint}/${categoryId}`;
+
 export const getCategories = () => http.get(apiEndpoint);
 
 export const createCategory = (name) => http.post(apiEndpoint, name);
 
 export const updateCategory = (categoryId, data) =>
-  http.patch(`${apiEndpoint}/${categoryId}`, data);
+  http.patch(categoryUrl(categoryId), data);
