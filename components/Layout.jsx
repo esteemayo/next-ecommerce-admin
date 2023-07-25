dd4e8312cca59aae8014af2aa99c16cc5581ef90
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 00:31:53 +0100

    fix: remove comments

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,17 +6,17 @@ import Sidebar from '@/components/Sidebar';
 const Layout = ({ children }) => {
   const { data: session } = useSession();
 
-  // if (!session) {
-  //   return (
-  //     <div className='bg-blue-900 w-screen h-screen flex items-center'>
-  //       <div className='text-center w-full'>
-  //         <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>
-  //           Login with Google
-  //         </button>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (!session) {
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='bg-blue-900 min-h-screen flex'>
