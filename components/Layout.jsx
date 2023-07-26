a0ce2ad99a9a44bc2adfbdecffefa1fcebf0e2d9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 03:44:48 +0100

    fix: modify

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useState } from 'react';
 import { signIn, useSession } from 'next-auth/react';
+
 import Sidebar from '@/components/Sidebar';
 
 const Layout = ({ children }) => {
