82ad3096aad00fb73b5d8194761614f93fcb5aad
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 19:10:14 +0100

    fix: rename function

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
   const { data: session } = useSession();
   const [showNav, setShowNav] = useState(false);
 
-  const handleOpen = useCallback((e) => {
+  const toggleButton = useCallback((e) => {
     e.stopPropagation();
     setShowNav((value) => !value);
   }, []);
@@ -30,7 +30,7 @@ const Layout = ({ children }) => {
   return (
     <div className='bg-bgGray min-h-screen'>
       <div className='md:hidden flex items-center p-4'>
-        <button onClick={handleOpen}>
+        <button onClick={toggleButton}>
           <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6'>
             <path fillRule='evenodd' d='M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z' clipRule='evenodd' />
           </svg>
