4be637526e0e67bd7380e93e841ac8ba74a78c54
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 03:41:02 +0100

    fix: add useCallback react hook

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { signIn, useSession } from 'next-auth/react';
 import Sidebar from '@/components/Sidebar';
 
