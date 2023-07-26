d1ce9095216796e0ff3952b5b9305b1589ebc34f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 01:16:42 +0100

    fix: add a middleware for admin authentication

---
+++ actions/getIsAdmin.js
@@ -0,0 +1,20 @@
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+
+const adminEmails = ['eadebayo15@gmail.com'];
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getIsAdmin() {
+  try {
+    const session = await getSession();
+
+    if (!adminEmails.includes(session?.user?.email)) {
+      throw new Error('Not admin');
+    }
+  } catch (err) {
+    return null;
+  }
+}
