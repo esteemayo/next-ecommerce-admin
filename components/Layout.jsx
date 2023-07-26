871bd41a7892a2fb48d06573c01872624c0fadba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-27 00:44:46 +0100

    fix: modify

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -49,7 +49,7 @@ const Layout = ({ children }) => {
       </div>
       <div className='flex'>
         <Sidebar show={showNav} />
-        <div className='bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
+        <div className='flex-grow mt-2 mr-2 mb-2 rounded-lg p-4'>
           {children}
         </div>
       </div>
