21a0d73094b8c5e589ee0267ef88e82cd670f760
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-07-26 19:09:15 +0100

    fix: modify

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
 
   const handleOpen = useCallback((e) => {
     e.stopPropagation();
-    setShowNav(true);
+    setShowNav((value) => !value);
   }, []);
 
   if (!session) {
