76d3f7257da49fb95b3147a22c737d2f6d7d336e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-01 15:50:28 +0100

    fix: add markup

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -4,6 +4,20 @@ const Orders = () => {
   return (
     <>
       <h1>Orders</h1>
+      <table className='basic'>
+        <thead>
+          <tr>
+            <th>Category name</th>
+            <th>Recipient</th>
+            <th>Products</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
