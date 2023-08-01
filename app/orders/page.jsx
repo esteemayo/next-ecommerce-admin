762b6fe2747638d78437d8ad9a8e91be0faeaf0a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-01 16:06:09 +0100

    fix: import getOrders

--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { getOrders } from '@/services/orderService';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
