2b3b03fc23e93a4a63ea8b46f7576733014706f7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 16:05:21 +0100

    fix: add ClientOnly component

--- app/products/new/page.jsx
+++ app/products/new/page.jsx
@@ -5,10 +5,10 @@ import ProductForm from '@/components/ProductForm';
 
 const NewProduct = () => {
   return (
-    <>
+    <ClientOnly>
       <h1>New Product</h1>
       <ProductForm />
-    </>
+    </ClientOnly>
   );
 };
 
