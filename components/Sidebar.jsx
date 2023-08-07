2868e7029f3ae93e2fcb954084eca0e82ffdd58a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 01:58:23 +0100

    fix: format code

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -63,7 +63,12 @@ const Sidebar = ({ show, onClose }) => {
           </svg>
           Categories
         </Link>
-        <Link onClick={onClose} href='/orders' passHref className={pathname.includes('/orders') ? activeLink : inactiveLink}>
+        <Link
+          onClick={onClose}
+          href='/orders'
+          passHref
+          className={pathname.includes('/orders') ? activeLink : inactiveLink}
+        >
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className={pathname.includes('/orders') ? activeIcon : inactiveIcon}>
             <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z' />
           </svg>
