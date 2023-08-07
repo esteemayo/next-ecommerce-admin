7932e254c612517b93fb8be212e4c39e90914d6a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 16:00:26 +0100

    fix: add ClientOnly component

--- app/page.jsx
+++ app/page.jsx
@@ -9,21 +9,23 @@ const Home = () => {
   const { data: session } = useSession();
 
   return (
-    <div className='text-blue-900 flex justify-between'>
-      <h2>Hello, <b>{session?.user?.name}</b></h2>
-      <div className='flex align-middle gap-1 bg-gray-300 text-black rounded-lg overflow-hidden'>
-        <Image
-          src={session?.user?.image}
-          width={6}
-          height={6}
-          alt=''
-          className='relative w-6 h-6'
-        />
-        <span className='px-2'>
-          {session?.user?.name}
-        </span>
+    <ClientOnly>
+      <div className='text-blue-900 flex justify-between'>
+        <h2>Hello, <b>{session?.user?.name}</b></h2>
+        <div className='flex align-middle gap-1 bg-gray-300 text-black rounded-lg overflow-hidden'>
+          <Image
+            src={session?.user?.image}
+            width={6}
+            height={6}
+            alt=''
+            className='relative w-6 h-6'
+          />
+          <span className='px-2'>
+            {session?.user?.name}
+          </span>
+        </div>
       </div>
-    </div>
+    </ClientOnly>
   );
 };
 
