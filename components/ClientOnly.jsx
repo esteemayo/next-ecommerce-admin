56450b2531bb4c67a00c84446b9df015b96a811f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 14:19:26 +0100

    fix: modify

--- components/ClientOnly.jsx
+++ components/ClientOnly.jsx
@@ -5,6 +5,10 @@ import { useEffect, useState } from 'react';
 const ClientOnly = () => {
   const [hasMounted, setHasMounted] = useState(false);
 
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
   return (
     <div>ClientOnly</div>
   );
