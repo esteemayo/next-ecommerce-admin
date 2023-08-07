9b90e819e32205a174a9fdd2f926de71e612a3bc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 13:52:41 +0100

    fix: refactor

--- services/httpService.js
+++ services/httpService.js
@@ -4,7 +4,7 @@ import { toast } from 'react-hot-toast';
 import logger from './logService';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
