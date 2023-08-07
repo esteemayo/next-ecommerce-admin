81d02a99f1bf00a550bd1e48d48068af66e72141
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 02:02:11 +0100

    fix: receive data via props

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 
-const Logo = () => {
+const Logo = ({ onClose }) => {
   return (
     <Link href='/' passHref className='flex gap-1'>
       <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
