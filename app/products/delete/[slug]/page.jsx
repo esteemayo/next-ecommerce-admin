26fca5e18d7bfbe071d944d6e6b29357b290abca
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 01:20:59 +0100

    fix: modify

--- app/products/delete/[slug]/page.jsx
+++ app/products/delete/[slug]/page.jsx
@@ -8,7 +8,6 @@ import { deleteProduct, getProductBySlug } from '@/services/productService';
 const DeleteProduct = ({ params }) => {
   const router = useRouter();
   const [product, setProduct] = useState({});
-  const [deletingId, setDeletingId] = useState(null);
 
   const goBack = useCallback(() => {
     router.push('/products');
