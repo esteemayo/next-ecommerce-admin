ed4d10311bd7b71799fa9addedbab1d19f62b0c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 01:12:00 +0100

    fix: modify

--- app/products/delete/[slug]/page.jsx
+++ app/products/delete/[slug]/page.jsx
@@ -50,6 +50,7 @@ const DeleteProduct = ({ params }) => {
           type='button'
           className='btn-red'
           onClick={handleDelete}
+          disabled={deletingId}
         >
           Yes
         </button>
