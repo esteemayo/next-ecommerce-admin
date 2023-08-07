4027f2967a3dc1ada7d0dd0d4a2b2c49ec0c4c3a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-08-07 02:26:28 +0100

    fix: add ToasterProvider

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Nunito } from 'next/font/google';
 
 import Layout from '@/components/Layout';
 import AuthProvider from '@/providers/AuthProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
 import './globals.css';
 
 const nunito = Nunito({
